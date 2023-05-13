7978fae961cb18aebde73b0ebc831a330d5f5181
refirio <refirio@gmail.com>
2023-05-13 21:27:20 +0900

    Initial Commit

---
+++ kidsbrowser/kidsbrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  kidsbrowserApp.swift
+//  kidsbrowser
+//
+//  Created by refirio.
+//
+
+import SwiftUI
+
+@main
+struct kidsbrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
